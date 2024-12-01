74bff4ed6760b4c583a900c12276066706f0b3a7
Hitriihoma <hitriihoma@gmail.com>
2024-12-02 01:01:24 +0500

    Обработка примеров данных в директории

--- code_package/process_data_dir.py
+++ code_package/process_data_dir.py
@@ -6,12 +6,23 @@ Created on Sat Nov 30 15:51:51 2024
 """
 
 import os
+import sys
 import json
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
 
-def process_data_dir(wdir=str(os.path.join(os.path.dirname(os.getcwd()), 'data'))):
+try:
+    from smoother.code_package.moving_average import MovingAverage
+except:
+    print('------------- need to add directory smoother to $PATH or PYTHONPATH----------')
+    add_to_path = str(os.path.dirname(os.path.dirname(os.getcwd()))) #str(os.getcwd()) # str(os.path.dirname(os.path.dirname(os.getcwd())))
+    sys.path.append(add_to_path)
+    from smoother.code_package.moving_average import MovingAverage
+
+def process_data_dir(wdir=str(os.path.join(os.path.dirname(os.getcwd()), 'data')), window=None):
     '''
-    Обработать дан
+    Обработать данные в директории
 
     Parameters
     ----------
@@ -26,6 +37,12 @@ def process_data_dir(wdir=str(os.path.join(os.path.dirname(os.getcwd()), 'data')
     # проверить наличие рабочей директории
     if not os.path.isdir(wdir):
         raise ValueError('Указанный путь к файлам не является директорией')
+    # проверить корректность введённых параметров
+    if window is None:
+        raise ValueError('Задайте параметры для обработки (окно)')
+    
+    # создать обработчик данных
+    smoother = MovingAverage(window)
     
     # список файлов в рабочей директории
     data_files = os.listdir(wdir)
@@ -45,6 +62,21 @@ def process_data_dir(wdir=str(os.path.join(os.path.dirname(os.getcwd()), 'data')
         # Выборки для обработки
         x = df['date'].to_numpy()
         y = df['x'].to_numpy()
-    
+        
+        # Обработать выборки
+        x_tr, y_tr = smoother.transform(x=x, y=y)
+        
+        # построить график, сохранить в ./results
+        save_dir = str(os.path.join(os.path.dirname(os.getcwd()), 'results'))
+        fig = plt.figure(figsize=(10,20))
+        ax = fig.add_subplot(111)
+        ax.plot(x, y,  marker='o', label='Исходные данные')
+        ax.plot(x_tr, y_tr,  marker='o', label='Обработанные данные')
+        ax.legend()
+        plt.savefig(os.path.join(save_dir,f'{os.path.splitext(filename)[0]}.png'))
+        plt.close()
+        
 if __name__ == "__main__":
-    process_data_dir(wdir=str(os.path.join(os.path.dirname(os.getcwd()), 'data')))
+    print('Введите окно скользящего среднего:')
+    window = int(input())
+    process_data_dir(wdir=str(os.path.join(os.path.dirname(os.getcwd()), 'data')), window=window)
