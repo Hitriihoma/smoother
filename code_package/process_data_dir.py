7e890ed2abdfaf70481501f98edc195f0b40a29e
Hitriihoma <hitriihoma@gmail.com>
2025-02-04 00:20:50 +0500

    Несколько сглаживателей на одном наборе данных и графике

--- code_package/process_data_dir.py
+++ code_package/process_data_dir.py
@@ -13,13 +13,17 @@ from matplotlib import pyplot as plt
 
 try:
     from smoother.code_package.moving_average import MovingAverage
+    from smoother.code_package.single_exponential_smoothing import SES
+    from smoother.code_package.double_exponential_smoothing import DES
 except:
     print('------------- need to add directory smoother to $PATH or PYTHONPATH----------')
     add_to_path = str(os.path.dirname(os.path.dirname(os.getcwd()))) #str(os.getcwd()) # str(os.path.dirname(os.path.dirname(os.getcwd())))
     sys.path.append(add_to_path)
     from smoother.code_package.moving_average import MovingAverage
+    from smoother.code_package.single_exponential_smoothing import SES
+    from smoother.code_package.double_exponential_smoothing import DES
 
-def process_data_dir(wdir=str(os.path.join(os.path.dirname(os.getcwd()), 'data')), window=None, drop_outlier=False):
+def process_data_dir(wdir=str(os.path.join(os.path.dirname(os.getcwd()), 'data')), smoother=None):
     '''
     Обработать данные в директории
 
@@ -40,15 +44,21 @@ def process_data_dir(wdir=str(os.path.join(os.path.dirname(os.getcwd()), 'data')
     # проверить наличие рабочей директории
     if not os.path.isdir(wdir):
         raise ValueError('Указанный путь к файлам не является директорией')
-    # проверить корректность введённых параметров
-    if window is None:
-        raise ValueError('Задайте параметры для обработки (окно)')
-    
-    # создать обработчик данных
-    smoother = MovingAverage(window)
-    
     # список файлов в рабочей директории
     data_files = os.listdir(wdir)
+    # Директория сохранения
+    save_dir = str(os.path.join(os.path.dirname(os.getcwd()), 'results'))
+    
+    # Создать обработчики
+    smoother_objs = {}
+    for smoother_name in smoother.keys():
+        smoother_type = smoother[smoother_name]["type"]
+        if smoother_type == "moving_average":
+            smoother_objs.update({smoother_name: MovingAverage(smoother[smoother_name]["window"], smoother[smoother_name]["drop_outliers"])})
+        elif smoother_type == "single_exponential":
+            smoother_objs.update({smoother_name: SES(smoother[smoother_name]["alpha"])})
+        elif smoother_type == "double_exponential":
+            smoother_objs.update({smoother_name: DES(smoother[smoother_name]["alpha"], smoother[smoother_name]["gamma"])})
     
     # обработать каждый файл из списка
     for filename in data_files:
@@ -67,15 +77,16 @@ def process_data_dir(wdir=str(os.path.join(os.path.dirname(os.getcwd()), 'data')
         x = df['date'].to_numpy()
         y = df['x'].to_numpy()
         
-        # Обработать выборки
-        x_tr, y_tr = smoother.transform(x=x, y=y, drop_outlier=drop_outlier)
-        
-        # построить график, сохранить в ./results
-        save_dir = str(os.path.join(os.path.dirname(os.getcwd()), 'results'))
-        fig = plt.figure(figsize=(30,10))
+        # Инициализация фигуры графика
+        fig = plt.figure(figsize=(30,10), facecolor=(1,1,1))
+        # Исходные данные
         ax = fig.add_subplot(111)
         ax.plot(x, y,  marker='o', label='Исходные данные')
-        ax.plot(x_tr, y_tr,  marker='o', label='Обработанные данные')
+        # Обработанные данные, используюя ранее созданные обработчики
+        for smoother_name in smoother_objs.keys():
+            temp_smoother = smoother_objs[smoother_name]
+            x_tr, y_tr = temp_smoother.transform(x=x, y=y)
+            ax.plot(x_tr, y_tr,  marker='o', alpha=0.5, label=f"Обработанные данные: {smoother_name}")
         ax.legend(fontsize=20)
         ax.grid(axis='y')
         ax.set_title(name, fontsize=30)
@@ -83,12 +94,9 @@ def process_data_dir(wdir=str(os.path.join(os.path.dirname(os.getcwd()), 'data')
         plt.close()
         
 if __name__ == "__main__":
-    window = int(input('Введите окно скользящего среднего:\n'))
-    drop_outlier_input = input('Требуется ли убирать выброс в подвыборках [True, False]:\n')
-    if drop_outlier_input == 'True':
-        drop_outlier = True
-    elif drop_outlier_input == 'False':
-        drop_outlier = False
-    else:
-        raise ValueError('Некорректное значение drop_outlier_input (требуется ли убирать выбросы')
-    process_data_dir(wdir=str(os.path.join(os.path.dirname(os.getcwd()), 'data')), window=window, drop_outlier=drop_outlier)
+    smoother = {"ma_10_true": {"type": "moving_average", "window": 10, "drop_outliers": True}
+                 ,"ma_10_false": {"type": "moving_average", "window": 10, "drop_outliers": False}
+                 ,"des_0.3_0.3": {"type": "double_exponential", "alpha": 0.3, "gamma": 0.3}
+                 ,"des_0.3_0.9": {"type": "double_exponential", "alpha": 0.3, "gamma": 0.9}
+                 }
+    process_data_dir(wdir=str(os.path.join(os.path.dirname(os.getcwd()), 'data')), smoother=smoother)
